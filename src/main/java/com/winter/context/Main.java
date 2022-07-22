c39b0be9481f1532a7666c262d40a66d64eddedb
davit.gevorgyan <davit.gevorgyan@teamsolutions.am>
2022-07-22 12:42:54 +0400

    edited ClassGenerator

--- src/main/java/com/winter/context/Main.java
+++ src/main/java/com/winter/context/Main.java
@@ -1,42 +1,29 @@
 package com.winter.context;
 
-import com.sun.codemodel.JCodeModel;
 import com.winter.context.annotation.Autowired;
 import com.winter.context.annotation.Bean;
 import com.winter.context.service.BeanCreatorService;
 import com.winter.context.util.ClassGenerator;
-import org.jsonschema2pojo.*;
-import org.jsonschema2pojo.rules.RuleFactory;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.net.URL;
 
 @Bean
 public class Main {
 
-    @Autowired
-    public static ClassGenerator classGenerator;
-
     public static void main(String[] args) throws Exception {
         Context context = new Context("com");
         context.start();
-        System.out.println(BeanCreatorService.beans.containsKey("main"));
+
 
         String filePath = "C:\\Users\\Davit.gevorgyan\\IdeaProjects\\winter\\src\\main\\java\\com\\winter\\context\\generatedClasses";
-        String className = "Userrr";
-        String json = "{\n" +
-                "        \"javaType\" : \"Context\",\n" +
-                "        \"type\" : \"object\"\n" +
-                "    }";
-//        classGenerator.generateClassFromJson(filePath,className,json);
-//
-//        String x = "username:string/password:string/id:int/name:string/surname:string/active:boolean/object:object";
-//        classGenerator.generateClassFromXProtocol(filePath, className, x);
+        String packagePath = "com.winter.context.generatedClasses";
+        String projectRootPath = "com.winter.context";
 
-        File file = new File("C:\\Users\\Davit.gevorgyan\\IdeaProjects\\winter\\src\\main\\java\\com\\winter\\context\\generatedClasses\\" + className + ".java");
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
-        fileOutputStream.write(12211);
+
+        String example = "username:String/password:String/id:int/active:boolean/context:Context/v:Integer/c:ClassGenerator/ss:Scanner";
+        String className = "User";
+
+        ClassGenerator classGenerator = new ClassGenerator(projectRootPath,filePath,packagePath);
+        classGenerator.generateClassFromXProtocol(className,example);
 
     }
 }
--- src/main/java/com/winter/context/util/ClassGenerator.java
+++ src/main/java/com/winter/context/util/ClassGenerator.java
@@ -1,104 +1,83 @@
 package com.winter.context.util;
 
-import com.sun.codemodel.JCodeModel;
 import com.winter.context.annotation.Bean;
-import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
-import org.jsonschema2pojo.*;
-import org.jsonschema2pojo.rules.RuleFactory;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.Scanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 @Bean
 public class ClassGenerator {
 
-    public static final String EXAMPLE = "username:string/password:string/id:int/active:boolean";
+    public static final String EXAMPLE = "username:String/password:String/id:int/active:boolean/context:Context";
+    public String projectRootPath;
+    public String filePath;
+    public String packagePath;
 
-    public void generateClassFromJson(String filePath, String className, String json) throws IOException, ComponentConfigurationException {
-        JCodeModel jcodeModel = new JCodeModel();
-        GenerationConfig config = new DefaultGenerationConfig() {
-            @Override
-            public boolean isGenerateBuilders() {
-                return true;
-            }
 
-            @Override
-            public SourceType getSourceType() {
-                return SourceType.JSON;
-            }
-
-        };
-        File outputJavaClassDirectory = new File(filePath);
-        SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator());
-        mapper.generate(jcodeModel, className, "", json);
-        jcodeModel.build(outputJavaClassDirectory);
+    public ClassGenerator(String projectRootPath, String filePath, String packagePath) {
+        this.projectRootPath = projectRootPath;
+        this.filePath = filePath;
+        this.packagePath = packagePath;
     }
 
-    public void generateClassFromXProtocol(String generationClassPath, String className, String context) throws IOException, ComponentConfigurationException {
-        List<String> params = new ArrayList<>();
-        String[] strings = context.split("/");
-        for (int i = 0; i < strings.length; i++) {
-            params.add("\"" + strings[i].split(":")[0] + "\"");
-            params.add("\"" + strings[i].split(":")[1] + "\"");
-        }
-        String j = "";
-        String json = "";
-        for (int i = 0; i < params.size(); i = i + 2) {
-            if (i == params.size() - 2) {
-                switch (params.get(i + 1)) {
-                    case "\"int\"":
-                        j = j + " " + params.get(i) + ":" + " " + "1" + " ";
-                        break;
-                    case "\"boolean\"":
-                        j = j + " " + params.get(i) + ":" + " " + "true" + " ";
-                        break;
-                    case "\"string\"":
-                        j = j + " " + params.get(i) + ":" + " " + params.get(i + 1) + " ";
-                        break;
-                    case "\"object\"":
-                        j = j + " " + params.get(i) + ":" + " " + "null" + " ";
-                        break;
-                }
-            } else {
-                switch (params.get(i + 1)) {
-                    case "\"int\"":
-                        j = j + " " + params.get(i) + ":" + " " + "1" + ",";
-                        break;
-                    case "\"boolean\"":
-                        j = j + " " + params.get(i) + ":" + " " + "true" + ",";
-                        break;
-                    case "\"string\"":
-                        j = j + " " + params.get(i) + ":" + " " + params.get(i + 1) + ",";
-                        break;
-                    case "\"object\"":
-                        j = j + " " + params.get(i) + ":" + " " + "null" + " ";
-                        break;
-                }
-            }
+    public ClassGenerator() {
 
-        }
-
-        json = "{" + j + "}";
-        System.out.println(json);
-        generateClassFromJson(generationClassPath, className, json);
     }
 
-    public void generateClassFromString(String generationClassPath, String className, String params) throws IOException {
-        File file = new File(generationClassPath);
+    public void generateClassFromXProtocol(String className, String params) throws IOException, ClassNotFoundException {
+
+        File file = new File(filePath + "\\" + className + ".java");
         FileOutputStream fileOutputStream = new FileOutputStream(file);
-        String context = "package com.winter.context.generatedClasses; \n" +
-                "public class " + className + " {\n" +
+        String imports = "";
+        String fields = "";
+        String context;
+
+        List<String> p = new ArrayList<>();
+        String[] s = params.split("/");
+        for (int i = 0; i < s.length; i++) {
+            p.add(s[i].split(":")[0]);
+            p.add(s[i].split(":")[1]);
+        }
+
+        for (int i = 0; i < p.size(); i = i + 2) {
+            fields = fields + "\n" + "    private " + p.get(i + 1) + " " + p.get(i) + ";";
+            if (!(p.get(i + 1).equals("int") || p.get(i + 1).equals("long") || p.get(i + 1).equals("short") || p.get(i + 1).equals("byte") || p.get(i + 1).equals("float") || p.get(i + 1).equals("double") || p.get(i + 1).equals("boolean") || p.get(i + 1).equals("char") || p.get(i + 1).equals("String"))) {
+                Reflections reflections = new Reflections(projectRootPath,new SubTypesScanner(false));
+                Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
+                for (Class c:
+                     classes) {
+                    if (c.getSimpleName().equals(p.get(i + 1))) {
+                        imports = imports + "\n" + "import " + c.getPackageName() + "." + c.getSimpleName() + ";";
+                    }
+                }
+
+            }
+
+        }
+        context = "package " + packagePath + ";" + "\n\n\n" +
+                "import lombok.Data;" + "\n" +
+                imports +
+                "\n\n" + "@Data" + "\n" +
+                "public class " + className + " {" +
+                "\n" + fields + "\n\n" +
                 "}";
+
         byte[] b = context.getBytes();
         fileOutputStream.write(b);
-
+        fileOutputStream.close();
 
     }
-
-
 }
 
