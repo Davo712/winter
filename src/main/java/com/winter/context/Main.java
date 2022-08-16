59f6484ae99aeedd81928056afee7067cc3f6d5f
davit.gevorgyan <davit.gevorgyan@teamsolutions.am>
2022-08-16 10:32:59 +0400

    Winter 2.0

--- src/main/java/com/winter/context/Main.java
+++ src/main/java/com/winter/context/Main.java
@@ -21,14 +21,15 @@ public class Main {
         DbConnector dbConnector = new DbConnector("banandb", "root", "", "localhost:3306");
         String y = "add user (id=16,active=true,balance=10)";
         String x = "get user";
-        String updateQUery = "update user id=16 (activation_code='assasa',balance=100,name='duq')";
+        String updateQUery = "update user id=17 (activation_code='assasa',balance=100,name='duq')";
         // dbConnector.execute(y,User.class);
         // User user = dbConnector.execute(y, User.class);
         // dbConnector.execute(updateQUery,User.class);
         //  System.out.println((User) dbConnector.execute(x, User.class, user));
 
 
-        System.out.println(dbConnector.execute("native update user set id = 11 where id = 10", User.class, new Object()));
+
+       List<User> list = (List<User>) dbConnector.execute("native update user set id = 11 where id = 10", User.class);
 
 
     }
